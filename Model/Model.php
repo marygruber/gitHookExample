e1f43c23862d2ce06c760c94dd4f77b67886f3f1
Maryele M T Gruber <maryele.gruber@madeiramadeira.com.br>
2021-02-24 11:47:47 -0300

    f

--- Model/Model.php
+++ Model/Model.php
@@ -1,17 +1,18 @@
 <?php
 
-class Model{
-    public function validaDados($usuario, $senha)
-    {
-        $variavelNaoUsada = true;
+namespace githoob_v01;
 
+class Model
+{
+
+    public function validaDados(string $usuario, string $senha): string
+    {
             /* Aplica a validação ao usuário e senha passados, utilizando as regras de négocio especificas para ele. */
         if (strlen($usuario) < 5) {
             return 'Digite o usu&aacute;rio corretamente';
         } else if (strlen($senha) < 8) {
             return 'A senha deve possuir mais de 8 caracteres';
-        } else {
-            return 'Login efetuado com sucesso';
         }
+            return 'Login efetuado com sucesso';
     }
 }
